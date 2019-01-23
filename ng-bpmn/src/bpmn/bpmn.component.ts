1bcb94cceba8f77819e1299532b84c17a308e194
Mario Mol <mariohmol@gmail.com>
2019-01-23 19:51:28 -0200

    v2 working

--- ng-bpmn/src/bpmn/bpmn.component.ts
+++ ng-bpmn/src/bpmn/bpmn.component.ts
@@ -8,7 +8,8 @@ import {
   Output,
   ViewChild,
   SimpleChanges,
-  EventEmitter
+  EventEmitter,
+  OnInit
 } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
@@ -23,13 +24,15 @@ import { Subscription, Observable } from 'rxjs';
  * Modeler - bootstraps a full-fledged BPMN editor
  */
 import BpmnJS from 'bpmn-js/lib/NavigatedViewer.js';
+import BpmnViewerJS from 'bpmn-js/lib/Viewer.js';
+import BpmnModelerJS from 'bpmn-js/lib/Modeler.js';
 // import ModelerBpmnJS from 'bpmn-js/dist/bpmn-modeler.production.min.js';
 // import BpmnJS from 'bpmn-js';
 
 import { importDiagram } from './lib';
 
 @Component({
-  selector: 'app-diagram',
+  selector: 'ng-bpmn',
   template: `
     <div #ref class="diagram-container"></div>
   `,
@@ -42,18 +45,25 @@ import { importDiagram } from './lib';
     `
   ]
 })
-export class BpmnEditorComponent implements AfterContentInit, OnChanges, OnDestroy {
+export class BpmnEditorComponent implements AfterContentInit, OnChanges, OnDestroy, OnInit {
   private bpmnJS: any; // BpmnJS
 
   @ViewChild('ref') public el: ElementRef;
   @Output() private importDone: EventEmitter<any> = new EventEmitter();
-
   @Input() public url: string;
+  @Input() public type: string; // viewer or modeler
 
   constructor(private http: HttpClient) {
+  }
 
-    this.bpmnJS = new BpmnJS();
-
+  ngOnInit() {
+    if (this.type === 'modeler') {
+      this.bpmnJS = new BpmnModelerJS();
+    } else if (this.type === 'viewer') {
+      this.bpmnJS = new BpmnViewerJS();
+    } else {
+      this.bpmnJS = new BpmnJS();
+    }
     this.bpmnJS.on('import.done', (data: any) => {
       if (!data.error) {
         this.bpmnJS.get('canvas').zoom('fit-viewport');
--- ng-bpmn/src/lib.module.ts
+++ ng-bpmn/src/lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BpmnEditorComponent } from './bpmn/bpmn.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [
     BpmnEditorComponent
